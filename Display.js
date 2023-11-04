c504d2c44c54bb1bc560fba77d7e45c44b44cb1d
travellercoder <guillermobukowski@gmail.com>
2023-11-04 10:59:08 -0300

    funcionalidad de botones borrar y reset en display

--- Display.js
+++ Display.js
@@ -3,15 +3,29 @@ class Display {
         this.displayValorActual = displayValorActual;
         this.displayValorAnterior = displayValorAnterior;
         this.calculadora = new Calculadora();
+        this.tipoDeOperacion = undefined
         this.valorActual = "";
         this.valorAnterior = ""
     }
 
     agregarNumero(numero) {
         if (numero === '.' && this.valorActual.includes('.')) return
-        this.valorActual = this.valorActual + numero; 
+        this.valorActual = this.valorActual.toString() + numero.toString(); 
         this.imprimirValores();
     }
+
+    clear() {
+        this.valorActual = this.valorActual.toString().slice(0 ,-1);
+        this.imprimirValores();
+    }
+
+    clearAll() {
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoDeOperacion = undefined;
+        this.imprimirValores()
+    }
+
 
     imprimirValores() {
         this.displayValorActual.textContent = this.valorActual;
